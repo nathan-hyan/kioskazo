bd620d0f8098a3e76d58ed3fe64b2f431c2bef23
Exequiel Mleziva <aldo.exequiel.mleziva@hp.com>
2022-11-22 08:40:51 -0300

    Expanded product model

--- src/interfaces/product.ts
+++ src/interfaces/product.ts
@@ -1,18 +1,43 @@
 import { ObjectId, Document } from 'mongoose';
 
+interface Variants {
+  color: string;
+  barCode: string;
+  stock: number;
+}
+
+interface Price {
+  list: number;
+  onlineStore: number;
+  cash: number;
+  cost: number;
+}
+
+interface Dimensions {
+  height: number;
+  width: number;
+  depth: number;
+}
 export interface Product extends Document {
   name: string;
-  internalId: number;
-  price: {
-    list: number;
-    onlineStore: number;
-    cash: number;
-    cost: number;
-  };
   stock: number;
-  category: string;
-  image: string;
   barcode: string;
+  category: string;
+  provider: string;
+  brand: string;
+  businessOwner: string;
+  dimensions: Dimensions;
+  weight: number;
   storeBranch: string;
   storeId: ObjectId;
+  storePosition: string;
+  description: string;
+  specifications: {
+    title: string;
+    description: string;
+  }[];
+  variants: Variants[];
+  price: Price;
+  internalId: number;
+  image: string;
 }
--- src/models/Products/index.ts
+++ src/models/Products/index.ts
@@ -4,6 +4,35 @@ import { model, Schema } from 'mongoose';
 const Products: Schema = new Schema(
   {
     name: { type: String, required: true },
+    stock: { type: Number, required: false, default: 0 },
+    barcode: { type: String, required: true },
+    category: { type: String, required: true },
+    provider: { type: String, required: false },
+    brand: { type: String, required: false },
+    businessOwner: { type: String, required: false },
+    dimensions: {
+      height: { type: Number, required: false, default: 0 },
+      width: { type: Number, required: false, default: 0 },
+      depth: { type: Number, required: false, default: 0 },
+    },
+    weight: { type: Number, required: false, default: 0 },
+    storeBranch: { type: String, required: true },
+    storeId: { type: Schema.Types.ObjectId, ref: 'Stores', required: true },
+    storePosition: { type: String, required: false, default: '' },
+    description: { type: String, required: false, default: '' },
+    specifications: [
+      {
+        title: { type: String, required: false, default: '' },
+        description: { type: String, required: false, default: '' },
+      },
+    ],
+    variants: [
+      {
+        color: { type: String, required: false, default: '' },
+        barCode: { type: String, required: false, default: '' },
+        stock: { type: Number, required: false, default: 0 },
+      },
+    ],
     price: {
       list: { type: Number, required: true, default: 0 },
       onlineStore: { type: Number, required: true, default: 0 },
@@ -11,12 +40,7 @@ const Products: Schema = new Schema(
       cost: { type: Number, required: true, default: 0 },
     },
     internalId: { type: Number, required: true, default: 0 },
-    stock: { type: Number, required: false, default: 0 },
-    category: { type: String, required: true },
     image: { type: String, required: false },
-    barcode: { type: String, required: true },
-    storeId: { type: Schema.Types.ObjectId, ref: 'Stores', required: true },
-    storeBranch: { type: String, required: true },
   },
   { timestamps: true },
 );
